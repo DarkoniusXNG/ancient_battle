4ce7be8dcbd5aff0ef71ad7b1e25f411fcd856a1
Darko V <darko1290@gmail.com>
2019-09-10 18:33:27 +0200

    Reworking items
    * Fixed item costs and some stats
    * Nerfed ATMA
    * Nerfed Rod of Atos
    * Nerfed Slippers of Halcyon ms, buffed radius
    * Nerfed Drum of Vitality, fixed its stats
    * 2 new items: Poor Man's Shield and Ring of Aquila.

---
+++ game/scripts/vscripts/items/custom_poor_mans_shield.lua
@@ -0,0 +1,100 @@
+﻿LinkLuaModifier("modifier_custom_poor_mans_shield_passives", "items/custom_poor_mans_shield.lua", LUA_MODIFIER_MOTION_NONE)
+
+item_custom_poor_mans_shield = class({})
+
+function item_custom_poor_mans_shield:GetIntrinsicModifierName()
+	return "modifier_custom_poor_mans_shield_passives"
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_custom_poor_mans_shield_passives = class({})
+
+function modifier_custom_poor_mans_shield_passives:IsHidden()
+	return true
+end
+
+function modifier_custom_poor_mans_shield_passives:IsDebuff()
+	return false
+end
+
+function modifier_custom_poor_mans_shield_passives:IsPurgable()
+	return false
+end
+
+function modifier_custom_poor_mans_shield_passives:GetAttributes()
+	return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+function modifier_custom_poor_mans_shield_passives:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+		MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK
+		--MODIFIER_EVENT_ON_TAKEDAMAGE
+	}
+end
+
+function modifier_custom_poor_mans_shield_passives:GetModifierBonusStats_Agility()
+	return self:GetAbility():GetSpecialValueFor("bonus_agility")
+end
+--[[ -- This is not good!
+if IsServer() then
+	function modifier_custom_poor_mans_shield_passives:OnTakeDamage(event)
+		local parent = self:GetParent()
+		local ability = self:GetAbility()
+		local target = event.target
+		local attacker = event.attacker
+
+		if parent ~= target then
+			return
+		end
+		
+		if event.damage < 0 then
+			return
+		end
+		
+		if event.damage_type ~= DAMAGE_TYPE_PHYSICAL then
+			return
+		end
+		
+		if HasBit(event.damage_flags, DOTA_DAMAGE_FLAG_BYPASSES_BLOCK) then
+			return
+		end
+
+		-- To prevent crashes:
+		if not attacker then
+			return
+		end
+
+		if attacker:IsNull() then
+			return
+		end
+
+		local chance = ability:GetSpecialValueFor("block_chance")
+
+		if ability:PseudoRandom(chance) then
+			self.block_bool = true
+		else
+			self.block_bool = false
+		end
+	end
+end
+]]
+
+function modifier_custom_poor_mans_shield_passives:GetModifierPhysical_ConstantBlock()
+	local parent = self:GetParent()
+	local ability = self:GetAbility()
+	local chance = ability:GetSpecialValueFor("block_chance")
+
+	if IsServer() then
+		if ability:PseudoRandom(chance) then
+			if parent:IsRangedAttacker() then
+				return ability:GetSpecialValueFor("damage_block_ranged")
+			else
+				return ability:GetSpecialValueFor("damage_block_melee")
+			end
+		end
+	end
+
+	return 0
+end
---
+++ game/scripts/vscripts/items/custom_ring_of_aquila.lua
@@ -0,0 +1,67 @@
+﻿LinkLuaModifier("modifier_custom_ring_of_aquila_passives", "items/custom_ring_of_aquila.lua", LUA_MODIFIER_MOTION_NONE)
+
+item_custom_ring_of_aquila = class({})
+
+function item_custom_ring_of_aquila:GetIntrinsicModifierName()
+	return "modifier_custom_ring_of_aquila_passives"
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_custom_ring_of_aquila_passives = class({})
+
+function modifier_custom_ring_of_aquila_passives:IsHidden()
+	return true
+end
+
+function modifier_custom_ring_of_aquila_passives:IsDebuff()
+	return false
+end
+
+function modifier_custom_ring_of_aquila_passives:IsPurgable()
+	return false
+end
+
+function modifier_custom_ring_of_aquila_passives:GetAttributes()
+	return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+function modifier_custom_ring_of_aquila_passives:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
+		MODIFIER_PROPERTY_MANA_REGEN_CONSTANT,
+		MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+		MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT
+	}
+end
+
+function modifier_custom_ring_of_aquila_passives:GetModifierPreAttack_BonusDamage()
+	return self:GetAbility():GetSpecialValueFor("bonus_damage")
+end
+
+function modifier_custom_ring_of_aquila_passives:GetModifierConstantManaRegen()
+	return self:GetAbility():GetSpecialValueFor("bonus_mana_regen")
+end
+
+function modifier_custom_ring_of_aquila_passives:GetModifierPhysicalArmorBonus()
+	return self:GetAbility():GetSpecialValueFor("bonus_armor")
+end
+
+function modifier_custom_ring_of_aquila_passives:GetModifierBonusStats_Strength()
+	return self:GetAbility():GetSpecialValueFor("bonus_strength")
+end
+
+function modifier_custom_ring_of_aquila_passives:GetModifierBonusStats_Agility()
+	return self:GetAbility():GetSpecialValueFor("bonus_agility")
+end
+
+function modifier_custom_ring_of_aquila_passives:GetModifierBonusStats_Intellect()
+	return self:GetAbility():GetSpecialValueFor("bonus_intellect")
+end
+
+function modifier_custom_ring_of_aquila_passives:GetModifierAttackSpeedBonus_Constant()
+	return self:GetAbility():GetSpecialValueFor("bonus_attack_speed")
+end
