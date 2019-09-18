0d971858537a09e1d6d3291dd92408ce6aa8058f
Darko V <darko1290@gmail.com>
2019-09-18 22:34:47 +0200

    Reworking 10
    * Added Lich tooltips
    * Lich sound fixes
    * Buffed Dark Ranger Dark minions damage
    * Buffed Fire Lord Lava Spawn damage and hp.
    * Buffed Lich Dark Ritual cooldown and mana cost.
    * Lich Death and Decay mana cost nerfed, buffed duration.
    * Lich Freezing Touch buffed slow values.
    * Lich Ice Storm buffed total damage and duration.
    * Lifestealer fixes
    * Lifestealer added better talents.
    * Atma: reduced recipe cost from 100 to 5.
    * Blademaster lvl 25 talent changed from +20 AGI to +600 HP.

--- game/resource/tooltips_russian/generate_tooltips.bat
+++ game/resource/tooltips_russian/generate_tooltips.bat
@@ -2,7 +2,7 @@ CLS
 @ECHO OFF
 ECHO.
 ECHO.
-COPY *.txt "D:\SteamLibrary\SteamApps\common\dota 2 beta\game\dota_addons\weird_dota\resource\tooltips_russian.txt"
+COPY *.txt "C:\Github\ancient_battle\game\resource\tooltips_russian.txt"
 PAUSE
 CLS
 EXIT
