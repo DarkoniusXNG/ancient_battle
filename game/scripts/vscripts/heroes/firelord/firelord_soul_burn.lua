11e1b616e7a9ac69f40002b990f75bcf80c59594
Darko V <darko1290@gmail.com>
2019-12-27 01:21:25 +0100

    Fire Lord changes
    * Flaming Presence damage reduced from 35 to 32.
    * Incinerate debuff duration reduced from 10 to 8 seconds.
    * Incinerate damage per stack reduced from 7/10/12/15 to 6/9/12/15.
    * Incinerate explosion damage reduced from 75/100/130/170 to 75/100/125/150.
    * Meteor Push mana cost reduced from 100 to 90.
    * Soul Burn cast range increased from 700 to 700/750/800/850.
    * Soul Burn cooldown rescaled from 15/14/13/12 to 14 seconds.
    * Soul Burn cooldown when cast on allies or self is now 9/8/7/6 seconds.
    * Summon Lava Spawn mana cost increased from 50 to 90.
    * Volcano: Added AoE indicator that helps you see which area is affected..
    * Volcano cooldown reduced from 160 to 120 seconds.
    * Volcano mana cost increased from 200/180/150 to 200/250/300.
    * Volcano effect radius increased from 700 to 725.
    * Volcano stun duration reduced from 1.5 to 1.3 seconds.
    * Volcano duration increased from 10 to 10/13/16 seconds. Total number of eruptions increased from 4 to 4/5/6.

--- game/scripts/vscripts/heroes/firelord/firelord_soul_burn.lua
+++ game/scripts/vscripts/heroes/firelord/firelord_soul_burn.lua
@@ -4,16 +4,27 @@ function SoulBurnStart(event)
 	local caster = event.caster
 	local ability = event.ability
 	local ability_level = ability:GetLevel() - 1
-	
+
 	local hero_duration = ability:GetLevelSpecialValueFor("hero_duration", ability_level)
 	local creep_duration = ability:GetLevelSpecialValueFor("creep_duration", ability_level)
-	
-	-- Checking if target is an enemy and if it has spell block
-	if not target:TriggerSpellAbsorb(ability) and target:GetTeamNumber() ~= caster:GetTeamNumber()  then
-		if target:IsRealHero() then
-			ability:ApplyDataDrivenModifier(caster, target, "modifier_soul_burn", {["duration"] = hero_duration})
-		else
-			ability:ApplyDataDrivenModifier(caster, target, "modifier_soul_burn", {["duration"] = creep_duration})
+
+	-- Checking if target is an enemy 
+	if target:GetTeamNumber() ~= caster:GetTeamNumber() then
+		-- Checking if target has spell block
+		if not target:TriggerSpellAbsorb(ability) then
+			if target:IsRealHero() then
+				ability:ApplyDataDrivenModifier(caster, target, "modifier_soul_burn", {["duration"] = hero_duration})
+			else
+				ability:ApplyDataDrivenModifier(caster, target, "modifier_soul_burn", {["duration"] = creep_duration})
+			end
 		end
+	else
+		local buff_duration = ability:GetLevelSpecialValueFor("buff_duration", ability_level)
+		local cooldown_allies = ability:GetLevelSpecialValueFor("cooldown_allies", ability_level)
+
+		ability:ApplyDataDrivenModifier(caster, target, "modifier_soul_buff", {["duration"] = buff_duration})
+		local cdr = caster:GetCooldownReduction()
+		ability:EndCooldown()
+		ability:StartCooldown(cdr*cooldown_allies)
 	end
 end
