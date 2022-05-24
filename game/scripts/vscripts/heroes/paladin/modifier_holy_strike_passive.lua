a1807b8925fb6617aea03451b6c283861efb5f1c
Darko V <darko1290@gmail.com>
2022-05-25 01:26:34 +0200

    Paladin
    * Guardian Angel buff duration reduced from 7/8/9/10 to 6.5/7/7.5/8 seconds.
    * Guardian Angel unit damage changed to 150/250/350/450 to killer's or Paladin's attack damage times a multiplier (1/1.5/2/2.5x).
    * Guardian Angel unit duration changed from 12/13/14/15 to Paladin's  respawn time with a minimum duration of 6.5/7/7.5/8 seconds.
    * Holy Strike bonus pure damage reduced from 50/60/110/140% to 60/70/80/90% of Paladin's attack damage.
    * Holy Strike cooldown reduced from 15/13/11/8 to 14/12/10/8 seconds.
    * Level 15 Talent changed from Global Guardian Angel to Wrath of God.
    * Level 20 Talent changed from Wrath of God to Global Eternal Devotion.
    * Added sounds to Holy Strike.

--- game/scripts/vscripts/heroes/paladin/modifier_holy_strike_passive.lua
+++ game/scripts/vscripts/heroes/paladin/modifier_holy_strike_passive.lua
@@ -33,7 +33,7 @@ function modifier_holy_strike_passive:OnRefresh()
 	local ability = self:GetAbility()
 	-- Add weapon glow effect only if the ability is off cooldown
 	if IsServer() then
-		if ability:IsCooldownReady() and (not parent:IsSilenced()) then
+		if ability:IsCooldownReady() and not parent:IsSilenced() and not parent:IsHexed() then
 			if ability:GetAutoCastState() == true then
 				parent:AddNewModifier(parent, ability, "modifier_holy_strike_passive_weapon_effect", {})
 			else
@@ -78,96 +78,113 @@ function modifier_holy_strike_passive:GetModifierAttackRangeBonus()
 	return 0
 end
 
-function modifier_holy_strike_passive:OnAttack(event)
-    local parent = self:GetParent()
-	local ability = self:GetAbility()
+if IsServer() then
+	function modifier_holy_strike_passive:OnAttack(event)
+		local parent = self:GetParent()
+		local ability = self:GetAbility()
+		local attacker = event.attacker
 
-	if event.attacker ~= parent then
-		return
+		-- Don't continue if the attacker doesn't exist
+		if not attacker or attacker:IsNull() then
+			return
+		end
+
+		-- Check if attacker has this modifier
+		if attacker ~= parent then
+			return
+		end
+
+		if parent:GetCurrentActiveAbility() ~= ability then
+			return
+		end
+
+		-- Manual cast detected
+		self.manual_cast = true
 	end
 
-	if parent:GetCurrentActiveAbility() ~= ability then
-		return
-	end
+	function modifier_holy_strike_passive:OnAttackLanded(event)
+		local parent = self:GetParent()
+		local ability = self:GetAbility()
+		local attacker = event.attacker
+		local target = event.target
 
-	-- Manual cast detected
-	self.manual_cast = true
-end
+		-- Don't continue if the attacker doesn't exist
+		if not attacker or attacker:IsNull() then
+			return
+		end
 
-function modifier_holy_strike_passive:OnAttackLanded(event)
-	local parent = self:GetParent()
-	local ability = self:GetAbility()
-	local target = event.target
+		-- Check if attacker has this modifier
+		if attacker ~= parent then
+			return
+		end
 
-	if event.attacker == parent and ability:IsCooldownReady() and (not parent:IsSilenced()) then
-		if ability:GetAutoCastState() == true then
-			-- The Attack while autocast is on
-			self:HolyStrike(event)
-		else
-			-- The Attack while autocast is off
-			if self.manual_cast then
+		if ability:IsCooldownReady() and not parent:IsSilenced() and not parent:IsHexed() then
+			if ability:GetAutoCastState() == true or self.manual_cast then
+				-- The Attack while autocast is on
 				self:HolyStrike(event)
 			end
 		end
 	end
-end
 
-function modifier_holy_strike_passive:HolyStrike(event)
-	if IsServer() and event then
-		local attacker = event.attacker or self:GetParent()
-		local target = event.target
-		local ability = self:GetAbility()	
-		local attack_damage = event.original_damage
+	function modifier_holy_strike_passive:HolyStrike(event)
+		if event then
+			local attacker = event.attacker or self:GetParent()
+			local target = event.target
+			local ability = self:GetAbility()	
+			local attack_damage = event.original_damage
 
-		-- To prevent crashes:
-		if not target then
-			return
+			-- Don't continue if attacked entity doesn't exist
+			if not target or target:IsNull() then
+				return
+			end
+
+			-- Check for existence of GetUnitName method to determine if target is a unit or an item
+			-- items don't have that method -> nil; if the target is an item, don't continue
+			if target.GetUnitName == nil then
+				return
+			end
+
+			-- If the attack target is a building or a ward then stop (return)
+			if target:IsTower() or target:IsBarracks() or target:IsBuilding() or target:IsOther() then
+				return
+			end
+
+			-- If the attacker is an illusion then stop (return)
+			if attacker:IsIllusion() then
+				return
+			end
+
+			-- Calculate bonus pure damage
+			local percent_as_pure = ability:GetSpecialValueFor("percent_damage_as_pure")
+			local true_damage = attack_damage*percent_as_pure/100
+
+			-- Sound on attacker
+			if true_damage > event.damage then
+				attacker:EmitSound("Hero_Omniknight.HammerOfPurity.Crit")
+			end
+
+			-- Sound on target
+			target:EmitSound("Hero_Omniknight.HammerOfPurity.Target")
+
+			-- Damage table
+			local damage_table = {}
+			damage_table.attacker = attacker
+			damage_table.damage = true_damage
+			damage_table.damage_type = DAMAGE_TYPE_PURE
+			damage_table.ability = ability
+			damage_table.victim = target
+
+			ApplyDamage(damage_table)
+
+			local player = attacker:GetPlayerOwner()
+			SendOverheadEventMessage(player, OVERHEAD_ALERT_BONUS_SPELL_DAMAGE, target, true_damage, player)
+
+			ability:UseResources(true, false, true)
+
+			-- Remove weapon glow effect
+			attacker:RemoveModifierByName("modifier_holy_strike_passive_weapon_effect")
+
+			self.manual_cast = nil
 		end
-
-		if target:IsNull() then
-			return
-		end
-
-		-- Check for existence of GetUnitName method to determine if target is a unit or an item
-		-- items don't have that method -> nil; if the target is an item, don't continue
-		if target.GetUnitName == nil then
-			return
-		end
-
-		-- If the attack target is a building or a ward then stop (return)
-		if target:IsTower() or target:IsBarracks() or target:IsBuilding() or target:IsOther() then
-			return
-		end
-
-		-- If the attacker is an illusion then stop (return)
-		if attacker:IsIllusion() then
-			return
-		end
-
-		-- Calculate bonus pure damage
-		local percent_as_pure = ability:GetSpecialValueFor("percent_damage_as_pure")
-		local true_damage = attack_damage*percent_as_pure/100
-
-		-- Damage table
-		local damage_table = {}
-		damage_table.attacker = attacker
-		damage_table.damage = true_damage
-		damage_table.damage_type = DAMAGE_TYPE_PURE
-		damage_table.ability = ability
-		damage_table.victim = target
-
-		ApplyDamage(damage_table)
-
-		local player = attacker:GetPlayerOwner()
-		SendOverheadEventMessage(player, OVERHEAD_ALERT_BONUS_SPELL_DAMAGE, target, true_damage, player)
-
-		--attacker:EmitSound("Hero_Kunkka.Tidebringer.Attack")
-
-		ability:UseResources(true, false, true)
-
-		-- Remove weapon glow effect
-		attacker:RemoveModifierByName("modifier_holy_strike_passive_weapon_effect")
-
-		self.manual_cast = nil
 	end
 end
