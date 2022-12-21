757bae244e703474cd4545dcaded60584645fa7d
Darko V <darko1290@gmail.com>
2022-12-21 20:02:20 +0100

    3)

--- game/scripts/vscripts/heroes/perun/electric_trap.lua
+++ game/scripts/vscripts/heroes/perun/electric_trap.lua
@@ -1,9 +1,6 @@
-if perun_electric_trap == nil then
-	perun_electric_trap = class({})
-end
+perun_electric_trap = class({})
 
 LinkLuaModifier("modifier_perun_electric_trap", "heroes/perun/electric_trap.lua", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_custom_electric_trap_stun", "heroes/perun/electric_trap.lua", LUA_MODIFIER_MOTION_NONE)
 
 function perun_electric_trap:GetAOERadius()
 	return self:GetSpecialValueFor("radius")
@@ -21,8 +18,9 @@ function perun_electric_trap:OnSpellStart()
 	caster:EmitSound("Hero_Techies.StasisTrap.Plant")
 
 	local trap_duration = self:GetSpecialValueFor("duration")
-	local trap = CreateUnitByName("npc_dota_techies_stasis_trap", point, true, caster, caster, caster:GetTeamNumber())
-	trap:SetOwner(caster)
+	local trap = CreateUnitByName("npc_dota_custom_electric_trap", point, true, caster, caster, caster:GetTeamNumber())
+	trap:SetOwner(caster:GetOwner())
+	trap:SetControllableByPlayer(caster:GetPlayerID(), true)
 	trap:SetDeathXP(12)
 	trap:SetMaximumGoldBounty(20)
 	trap:SetMinimumGoldBounty(10)
@@ -53,107 +51,111 @@ function modifier_perun_electric_trap:IsDebuff()
 end
 
 function modifier_perun_electric_trap:OnCreated()
-	if IsServer() then
-		self:StartIntervalThink(0.15)
-	end
+	if not IsServer() then return end
+	
+	self:StartIntervalThink(0.1)
 end
 
 function modifier_perun_electric_trap:OnIntervalThink()
+	if not IsServer() then return end
+	
+	local caster = self:GetCaster()
 	local ability = self:GetAbility()
+	if not ability or ability:IsNull() or not caster or caster:IsNull() then
+		-- Remove the mine
+		local parent = self:GetParent()
+		if parent and not parent:IsNull() then
+			parent:ForceKill(false)
+		end
+		-- Stop Interval think
+		self:StartIntervalThink(-1)
+		return
+	end
+
 	local radius = ability:GetSpecialValueFor("radius")
 
-	if IsServer() then
-		local parent = self:GetParent()
-		local mine_team = parent:GetTeamNumber()
-		local point = parent:GetAbsOrigin()
+	local parent = self:GetParent()
+	local team = parent:GetTeamNumber()
+	local point = parent:GetAbsOrigin()
 
-		if parent:IsOutOfGame() or parent:IsUnselectable() then
-			return
+	if parent:IsOutOfGame() or parent:IsUnselectable() or parent:IsInvulnerable() then
+		return
+	end
+
+	-- Targetting constants
+	local target_team = DOTA_UNIT_TARGET_TEAM_ENEMY
+	local target_type = bit.bor(DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_HERO)
+	local target_flags = DOTA_UNIT_TARGET_FLAG_NONE
+
+	local all_enemies = FindUnitsInRadius(team, point, nil, radius, target_team, target_type, target_flags, FIND_ANY_ORDER, false)
+	local number_of_valid_enemies = 0
+	for _, enemy in pairs(all_enemies) do
+		if enemy and not enemy:IsNull() and not enemy:IsCustomWardTypeUnit() then
+			number_of_valid_enemies = number_of_valid_enemies + 1
 		end
+	end
 
-		-- Targetting constants
-		local target_team = DOTA_UNIT_TARGET_TEAM_ENEMY
-		local target_type = bit.bor(DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_HERO)
-		local target_flags = DOTA_UNIT_TARGET_FLAG_NONE
+	if number_of_valid_enemies > 0 then
+		-- Stop Interval think
+		self:StartIntervalThink(-1)
 
-		local all_enemies = FindUnitsInRadius(mine_team, point, nil, radius, target_team, target_type, target_flags, FIND_ANY_ORDER, false)
-		local number_of_valid_enemies = 0
-		for _, enemy in pairs(all_enemies) do
-			if enemy then
-				local name = enemy:GetUnitName()
-				if name ~= "npc_dota_custom_electric_trap" and name ~= "npc_dota_techies_stasis_trap" then
-					number_of_valid_enemies = number_of_valid_enemies + 1
+		local delay = ability:GetSpecialValueFor("delay")
+		local damage = ability:GetSpecialValueFor("damage")
+		
+		-- Damage table
+		local damage_table = {}
+		damage_table.attacker = caster
+		damage_table.damage = damage
+		damage_table.damage_type = DAMAGE_TYPE_MAGICAL
+		damage_table.ability = ability
+
+		Timers:CreateTimer(delay, function()
+			if parent:IsNull() then
+				return
+			end
+			if parent:IsAlive() and not parent:IsOutOfGame() and not parent:IsUnselectable() and not parent:IsInvulnerable() then
+				local parent_team = parent:GetTeamNumber()
+				local parent_origin = parent:GetAbsOrigin()
+				local all = FindUnitsInRadius(parent_team, parent_origin, nil, radius, target_team, target_type, target_flags, FIND_ANY_ORDER, false)
+				local enemies_again = {}
+				for _, enemy in pairs(all) do
+					if enemy and not enemy:IsNull() and not enemy:IsCustomWardTypeUnit() then
+						table.insert(enemies_again, enemy)
+					end
+				end
+
+				if #enemies_again > 0 then
+
+					-- Hide parent
+					parent:AddNoDraw()
+
+					-- Stasis Trap particles
+					local particle_explode = "particles/units/heroes/hero_techies/techies_stasis_trap_explode.vpcf"
+					local particle_explode_fx = ParticleManager:CreateParticle(particle_explode, PATTACH_WORLDORIGIN, parent)
+					ParticleManager:SetParticleControl(particle_explode_fx, 0, parent:GetAbsOrigin())
+					ParticleManager:SetParticleControl(particle_explode_fx, 1, Vector(radius, 1, 1))
+					ParticleManager:SetParticleControl(particle_explode_fx, 3, parent:GetAbsOrigin())
+					ParticleManager:ReleaseParticleIndex(particle_explode_fx)
+
+					-- Stasis trap sound
+					parent:EmitSound("Hero_Techies.ReactiveTazer.Detonate")
+
+					for _, enemy in pairs(enemies_again) do
+						if not enemy:IsNull() then
+							-- Victim
+							damage_table.victim = enemy
+
+							-- Explode (damage)
+							ApplyDamage(damage_table)
+						end
+					end
+
+					parent:ForceKill(false)
+				else
+					self:StartIntervalThink(0.1)
 				end
 			end
-		end
-
-		if number_of_valid_enemies > 0 then
-			-- Stop Interval think
-			self:StartIntervalThink(-1)
-
-			local delay = ability:GetSpecialValueFor("delay")
-			local stun_duration = ability:GetSpecialValueFor("stun_duration")
-
-			Timers:CreateTimer(delay, function()
-				if parent:IsNull() then
-					return
-				end
-				if parent:IsAlive() and (not parent:IsOutOfGame()) and (not parent:IsUnselectable()) then
-					local parent_team = parent:GetTeamNumber()
-					local parent_origin = parent:GetAbsOrigin()
-					--local parent_death_xp = parent:GetDeathXP()
-					local all = FindUnitsInRadius(parent_team, parent_origin, nil, radius, target_team, target_type, target_flags, FIND_ANY_ORDER, false)
-					local enemies_again = {}
-					for _, enemy in pairs(all) do
-						if enemy then
-							local name = enemy:GetUnitName()
-							if name ~= "npc_dota_custom_electric_trap" and name ~= "npc_dota_techies_stasis_trap" then
-								table.insert(enemies_again, enemy)
-							end
-						end
-					end
-
-					if #enemies_again > 0 then
-
-						-- Hide parent
-						parent:AddNoDraw()
-
-						-- Stasis Trap particles
-						local particle_explode = "particles/units/heroes/hero_techies/techies_stasis_trap_explode.vpcf"
-						local particle_explode_fx = ParticleManager:CreateParticle(particle_explode, PATTACH_WORLDORIGIN, parent)
-						ParticleManager:SetParticleControl(particle_explode_fx, 0, parent:GetAbsOrigin())
-						ParticleManager:SetParticleControl(particle_explode_fx, 1, Vector(radius, 1, 1))
-						ParticleManager:SetParticleControl(particle_explode_fx, 3, parent:GetAbsOrigin())
-						ParticleManager:ReleaseParticleIndex(particle_explode_fx)
-
-						-- Stasis trap sound
-						parent:EmitSound("Hero_Techies.ReactiveTazer.Detonate")
-
-						for _, enemy in pairs(enemies_again) do
-							if enemy then
-								-- Status Resistance fix
-								local enemy_stun_duration = enemy:GetValueChangedByStatusResistance(stun_duration)
-								-- Apply Electric Trap stun debuff to the enemy
-								enemy:AddNewModifier(parent, ability, "modifier_custom_electric_trap_stun", {duration = enemy_stun_duration})
-							end
-						end
-
-						parent:ForceKill(false)
-
-						-- local hero_flags = bit.bor(DOTA_UNIT_TARGET_FLAG_DEAD, DOTA_UNIT_TARGET_FLAG_NOT_ILLUSIONS, DOTA_UNIT_TARGET_FLAG_NOT_CREEP_HERO)
-						-- local enemy_heroes_around = FindUnitsInRadius(parent_team, parent_origin, nil, 1500, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, hero_flags, FIND_ANY_ORDER, false)
-						-- local number_of_heroes = #enemy_heroes_around
-						-- for _, hero in pairs(enemy_heroes_around) do
-							-- if number_of_heroes > 0 then
-								-- hero:AddExperience(parent_death_xp/number_of_heroes, DOTA_ModifyXP_CreepKill, false, false)
-							-- end
-						-- end
-					else
-						self:StartIntervalThink(0.15)
-					end
-				end
-			end)
-		end
+		end)
 	end
 end
 
@@ -182,37 +184,4 @@ function modifier_perun_electric_trap:CheckState()
 		[MODIFIER_STATE_MAGIC_IMMUNE] = true,
 		[MODIFIER_STATE_CANNOT_BE_MOTION_CONTROLLED] = true,
 	}
-end
-
--- Stun modifier -----------------------------------------------------------------------------------------------------
-
-if modifier_custom_electric_trap_stun == nil then
-	modifier_custom_electric_trap_stun = class({})
-end
-
-function modifier_custom_electric_trap_stun:IsHidden()
-	return false
-end
-
-function modifier_custom_electric_trap_stun:IsDebuff()
-	return true
-end
-
-function modifier_custom_electric_trap_stun:IsStunDebuff()
-	return true
-end
-
-function modifier_custom_electric_trap_stun:IsPurgable()
-	return true
-end
-
-function modifier_custom_electric_trap_stun:CheckState()
-	return {
-		[MODIFIER_STATE_STUNNED] = true,
-		[MODIFIER_STATE_FROZEN] = true
-	}
-end
-
-function modifier_custom_electric_trap_stun:GetStatusEffectName()
-	return "particles/status_fx/status_effect_techies_stasis.vpcf"
 end
--- game/scripts/vscripts/libraries/basenpc.lua
+++ game/scripts/vscripts/libraries/basenpc.lua
@@ -29,16 +29,17 @@ if CDOTA_BaseNPC then
 
   function CDOTA_BaseNPC:IsCustomWardTypeUnit()
     local names = {
+      "npc_dota_custom_death_ward",
+      "npc_dota_custom_dummy_unit",
+      "npc_dota_custom_electric_trap",
+      "npc_dota_custom_phoenix_egg",
+      "npc_dota_firelord_volcano",
       "npc_dota_techies_custom_land_mine_moving",
       "npc_dota_techies_custom_remote_mine",
       "npc_dota_techies_custom_remote_mine_moving",
       "npc_dota_techies_custom_stasis_trap_moving",
       "npc_dota_techies_land_mine",
       "npc_dota_techies_stasis_trap",
-      "npc_dota_custom_death_ward",
-      "npc_dota_custom_dummy_unit",
-      "npc_dota_custom_phoenix_egg",
-      "npc_dota_firelord_volcano",
     }
     for _, v in pairs(names) do
       if self:GetUnitName() == v then
@@ -323,16 +324,17 @@ if C_DOTA_BaseNPC then
 
   function C_DOTA_BaseNPC:IsCustomWardTypeUnit()
     local names = {
+      "npc_dota_custom_death_ward",
+      "npc_dota_custom_dummy_unit",
+      "npc_dota_custom_electric_trap",
+      "npc_dota_custom_phoenix_egg",
+      "npc_dota_firelord_volcano",
       "npc_dota_techies_custom_land_mine_moving",
       "npc_dota_techies_custom_remote_mine",
       "npc_dota_techies_custom_remote_mine_moving",
       "npc_dota_techies_custom_stasis_trap_moving",
       "npc_dota_techies_land_mine",
       "npc_dota_techies_stasis_trap",
-      "npc_dota_custom_death_ward",
-      "npc_dota_custom_dummy_unit",
-      "npc_dota_custom_phoenix_egg",
-      "npc_dota_firelord_volcano",
     }
     for _, v in pairs(names) do
       if self:GetUnitName() == v then
